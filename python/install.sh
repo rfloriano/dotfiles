8667fdd3a60e4b77ce9e11a5f70546bafbd4199d
rfloriano <rflorianobr@gmail.com>
2021-02-26 12:08:32 -0300

    Python things

---
+++ python/install.sh
@@ -0,0 +1,5 @@
+pip3 install -r ./python/requirements.txt
+
+# pyenv install-latest plugin
+git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest
+
