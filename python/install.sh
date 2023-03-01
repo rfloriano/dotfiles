8f6e4d7e07d271855964e65155b5c5936fa531c6
rfloriano <rflorianobr@gmail.com>
2023-03-01 17:25:38 -0300

    hashcorp stuff and git for tucows

--- python/install.sh
+++ python/install.sh
@@ -2,4 +2,3 @@ pip3 install -r ./python/requirements.txt
 
 # pyenv install-latest plugin
 git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest
-
